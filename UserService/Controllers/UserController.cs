8e297dbfb30764142314b4717804bc5abb56e6e0
TTLoc <loctt.it2k@gmail.com>
2025-08-18 12:11:32 +0700

    Add project files.

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("api/user/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult GetByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
